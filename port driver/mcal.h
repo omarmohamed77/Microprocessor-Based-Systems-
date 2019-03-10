6ef671fac73c98d1891217a42cab92d10079007d
omar mohamed <omar.7.mohamed96@gmail.com>
2019-03-11 00:29:03 +0200

    port driver functions

---
+++ port driver/mcal.h
@@ -0,0 +1,17 @@
+/*
+ * port_int.h
+ *
+ *  Created on: Mar 10, 2019
+ *      Author: omar
+ */
+
+#ifndef MCAL_H_
+#define MCAL_H_
+
+#include "arm_config.h"
+#include "std_types.h"
+#include "common macros.h"
+void PORT_INT(uint8 port_index);
+
+
+#endif /* MCAL_H_ */
