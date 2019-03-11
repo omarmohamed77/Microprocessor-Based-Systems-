6c3d18267e21f9620f58ebafb7f5db21c9bbe43b
Omar Shafik <o.shafik13@gmail.com>
2019-03-11 17:57:42 +0200

    almost completed read port function

--- DIO_Driver/DIO.c
+++ DIO_Driver/DIO.c
@@ -5,6 +5,7 @@ uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask)
 
     //Port_SetPinDirection(port_index, pins_mask, PORT_PIN_IN);
     //Port_SetPinPullUp(port_index, pins_mask, 1);
+    volatile uint32 *PORT = GPIO_PORTA_DATA_BITS_R + ((port_index & 0x03) << 12) + ((port_index >> 2) << 17);
 
-    return GPIO_PORTA_DATA_R & pins_mask;
+    return PORT[pins_mask];
 }
