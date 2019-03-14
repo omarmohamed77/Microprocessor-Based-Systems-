5ae5d167587b2e48b0ad64533bffbb0e0e940cd1
omar fathey elsisy <31731092+omarElsisy@users.noreply.github.com>
2019-03-14 09:36:27 +0200

    Update Port_SetPinPullUp.c

--- Port_SetPinPullUp (func)/Port_SetPinPullUp.c
+++ Port_SetPinPullUp (func)/Port_SetPinPullUp.c
@@ -1,33 +1,59 @@
 #include "Port_SetPinPullUp"
 void Port_SetPinPullUp(uint8 port_index,uint8 pins_mask,uint8 enable){
-uint32 Port_reg;
 	switch(port_index){
-		case 0:
-			Port_reg=GPIO_PORTA_PUR_R;
+		case 0://case port A
+			if(enable){
+	GPIO_PORTA_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTA_PUR_R &= (~(pins_mask));
+            }
 	     break;
-		case 1:
-			Port_reg=GPIO_PORTB_PUR_R;
+		case 1://case portB
+			if(enable){
+	GPIO_PORTB_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTB_PUR_R &= (~(pins_mask));
+            }
 		   break;
-	case 2:
-			Port_reg=GPIO_PORTC_PUR_R;
+	case 2://case portC
+			
+			if(enable){
+	GPIO_PORTC_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTC_PUR_R &= (~(pins_mask));
+            }
 		   break;
 	
-	case 3:
+	case 3://case portD
+			
 			Port_reg=GPIO_PORTD_PUR_R;
+			if(enable){
+	GPIO_PORTD_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTC_PUR_R &= (~(pins_mask));
+            }
 		   break;
-	case 4:
+	case 4://case portE
 			Port_reg=GPIO_PORTE_PUR_R;
+	if(enable){
+	GPIO_PORTE_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTE_PUR_R &= (~(pins_mask));
+            }
 		   break;
-		case 5:
-			Port_reg=GPIO_PORTF_PUR_R;
+		case 5://case Portf
+			if(enable){
+	GPIO_PORTF_PUR_R |= pins_mask;
+	}
+        else{
+         GPIO_PORTF_PUR_R &= (~(pins_mask));
+            }
 		   break;
 	
 	}
-	if(enable){
-	Port_reg |= pins_mask;
-	}
-  else{
-   Port_reg &= (~(pins_mask));
-     }
-
 }
