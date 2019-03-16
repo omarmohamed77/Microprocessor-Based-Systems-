27140c2d35318f385931e2af4de394e401abfca2
Omar Shafik <o.shafik13@gmail.com>
2019-03-16 13:27:49 +0200

    complete dio read

--- DIO_Driver/DIO.h
+++ DIO_Driver/DIO.h
@@ -5,11 +5,6 @@
 #include "std_types.h"
 #include "common macros.h"
 
-/*
-steps to read from PORTX (assuming port is initialized):
-1. Set direction of masked port pins.
-2. set masked port pins as pull ups.
-*/
 uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask);
 
 #endif /* MCAL_H_ */
