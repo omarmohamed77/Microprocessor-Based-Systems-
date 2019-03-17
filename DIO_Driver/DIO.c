2070faa88becb4c1a2d9ea33460fc44398e13cf5
Omar Shafik <o.shafik13@gmail.com>
2019-03-17 17:43:47 +0200

    read port fixed

--- DIO_Driver/DIO.c
+++ DIO_Driver/DIO.c
@@ -2,32 +2,31 @@
 
 uint8 DIO_ReadPort(uint8 port_index, uint8 pins_mask)
 {
-    // volatile uint32 *PORT = GPIO_PORTA_DATA_BITS_R + ((port_index & 0x03) << 12) + ((port_index >> 2) << 17);
-    // return PORT[port_index];
-    // return (GPIO_PORTA_DATA_BITS_R + ((port_index & 0x03) << 12) + ((port_index >> 2) << 17))[pins_mask];
+    volatile uint32 *PORT = GPIO_PORTA_DATA_BITS_R + ((port_index & 0x03) << 10) + ((port_index >> 2) << 15);
+    return PORT[port_index];
 
-    switch (port_index)
-    {
-    case 0:
-        return (GPIO_PORTA_DATA_R & pins_mask);
-        break;
-    case 1:
-        return (GPIO_PORTB_DATA_R & pins_mask);
-        break;
-    case 2:
-        return (GPIO_PORTC_DATA_R & pins_mask);
-        break;
-    case 3:
-        return (GPIO_PORTD_DATA_R & pins_mask);
-        break;
-    case 4:
-        return (GPIO_PORTE_DATA_R & pins_mask);
-        break;
-    case 5:
-        return (GPIO_PORTF_DATA_R & pins_mask);
-        break;
+    // switch (port_index)
+    // {
+    // case 0:
+    //     return (GPIO_PORTA_DATA_R & pins_mask);
+    //     break;
+    // case 1:
+    //     return (GPIO_PORTB_DATA_R & pins_mask);
+    //     break;
+    // case 2:
+    //     return (GPIO_PORTC_DATA_R & pins_mask);
+    //     break;
+    // case 3:
+    //     return (GPIO_PORTD_DATA_R & pins_mask);
+    //     break;
+    // case 4:
+    //     return (GPIO_PORTE_DATA_R & pins_mask);
+    //     break;
+    // case 5:
+    //     return (GPIO_PORTF_DATA_R & pins_mask);
+    //     break;
 
-    default:
-        break;
-    }
+    // default:
+    //     break;
+    // }
 }
