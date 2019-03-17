c2e1eac3efc8a390b649a49948662ff1d94dee3d
Martina-osama <36206098+Martina-osama@users.noreply.github.com>
2019-03-18 01:08:42 +0200

    Add files via upload

---
+++ DIO_FlipPort/DIO_FlipPort.h
@@ -0,0 +1,10 @@
+/*Created on: Mar 18, 2019
+ * Martina
+ */
+ 
+# include "stdint.h"
+# include "tm4c123gh6pm.h"
+# include "std_types.h"
+# include "arm_config.h"
+# include "common macros.h"
+void DIO_FlipPort (uint8 port_index, uint8 pins_mask);
