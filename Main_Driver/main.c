608b1a90c57978edb887a60ea6618e0f6ab12c1c
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-03-18 04:58:59 +0200

    Update main.c

--- Main_Driver/main.c
+++ Main_Driver/main.c
@@ -1,16 +1,19 @@
 #include "mcal.h"
 #include "Button_Modes.h"
 
-#define PORTF_INDEX					5
-#define INPUT						  	0
-#define OUTPUT						  1
+#define PORTF_INDEX				5
+#define INPUT					0
+#define OUTPUT					1
+#define Increment				1
+#define Decrement				2
+#define RESET					3
 #define GPIO_PF321_M				0x0EU 	//Pins 3, 2 and 1 are connected to RGB LED
-#define GPIO_PF4_M					0x10U		// Pin 4 is connected to switch_1
-#define GPIO_PF3_M					0x08U		// Pin 3 is connected to green	
-#define GPIO_PF2_M					0x04U		// Pin 2 is connected to blue
-#define GPIO_PF1_M					0x02U		// Pin 1 is connected to red
-#define GPIO_PF0_M					0x01U		// Pin 4 is connected to switch_2
-#define GPIO_PF40_M					0x11U 	//Pins 0 and 4 are connected to Input Switches
+#define GPIO_PF4_M				0x10U		// Pin 4 is connected to switch_1
+#define GPIO_PF3_M				0x08U		// Pin 3 is connected to green	
+#define GPIO_PF2_M				0x04U		// Pin 2 is connected to blue
+#define GPIO_PF1_M				0x02U		// Pin 1 is connected to red
+#define GPIO_PF0_M				0x01U		// Pin 4 is connected to switch_2
+#define GPIO_PF40_M				0x11U 	//Pins 0 and 4 are connected to Input Switches
 
 void SystemInit(void){
 	PORT_INT(PORTF_INDEX);
@@ -21,7 +24,7 @@ void SystemInit(void){
 
 int main(void){
 	unsigned char data;
-	for(;;){
+	while(1){
 		data = DIO_ReadPort(PORTF_INDEX, GPIO_PF40_M);
 		if(data == GPIO_PF0_M){
 			Button_Operation_Mode(PORTF_INDEX, GPIO_PF4_M, 1);
