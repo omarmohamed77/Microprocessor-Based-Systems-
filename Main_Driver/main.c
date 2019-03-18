154d33f662d00a4eccc4bf3a89e487cbac1db417
omarmagdy217 <omr.magdy217@gmail.com>
2019-03-18 23:13:42 +0200

    full project main

--- Main_Driver/main.c
+++ Main_Driver/main.c
@@ -1,36 +1,72 @@
 #include "mcal.h"
-#include "Button_Modes.h"
+#include "LCD.h"
+#include "delay.h"
 
-#define PORTF_INDEX				5
-#define INPUT					0
-#define OUTPUT					1
-#define Increment				1
-#define Decrement				2
-#define RESET					3
-#define GPIO_PF321_M				0x0EU 	//Pins 3, 2 and 1 are connected to RGB LED
-#define GPIO_PF4_M				0x10U		// Pin 4 is connected to switch_1
-#define GPIO_PF3_M				0x08U		// Pin 3 is connected to green	
-#define GPIO_PF2_M				0x04U		// Pin 2 is connected to blue
-#define GPIO_PF1_M				0x02U		// Pin 1 is connected to red
-#define GPIO_PF0_M				0x01U		// Pin 4 is connected to switch_2
-#define GPIO_PF40_M				0x11U 	//Pins 0 and 4 are connected to Input Switches
+#define PORTA_INDEX					0
+#define PORTB_INDEX					1
+#define ENABLE						  1
+#define RST_BUTTON					0x10U		// Pin 4 is connected to increment switch
+#define DEC_BUTTON					0x08U		// Pin 3 is connected to decrement switch
+#define INC_BUTTON					0x04U		// Pin 2 is connected to Reset switch
+#define PUSH_BUTTONS				0x1CU 	//Pins 0 and 4 are connected to Input Switches
 
 void SystemInit(void){
-	PORT_INT(PORTF_INDEX);
-	Port_SetPinDirection(PORTF_INDEX, GPIO_PF321_M, OUTPUT);
-	Port_SetPinDirection(PORTF_INDEX, GPIO_PF40_M, INPUT);
-	Port_SetPinPullUp(PORTF_INDEX,GPIO_PF40_M, 1);
+	PORT_INT(PORTA_INDEX);
+	PORT_INT(PORTB_INDEX);
+	Port_SetPinDirection(PORTB_INDEX, 0xFF, PORT_PIN_OUT);
+	Port_SetPinDirection(PORTA_INDEX, PUSH_BUTTONS, PORT_PIN_IN);
+	Port_SetPinDirection(PORTA_INDEX, 0xE0, PORT_PIN_OUT);
+	LCD_init();
+	Port_SetPinPullUp(PORTA_INDEX,PUSH_BUTTONS, ENABLE);
 }
 
 int main(void){
-	unsigned char data;
+	uint8 data;
+	uint16 counter = 0;
+	LCD_data(48);
+	LCD_data(48);
+	LCD_data(48);
 	while(1){
-		data = DIO_ReadPort(PORTF_INDEX, GPIO_PF40_M);
-		if(data == GPIO_PF0_M){
-			Button_Operation_Mode(PORTF_INDEX, GPIO_PF4_M, Increment);
+		data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+		if(data == 0x18U){
+			delay(20);
+			data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			if(data == 0x18U){
+				// LCD Increment
+				counter = (counter+1)%1000;
+				LCD_command(CLEAR);
+				LCD_data((uint8)((counter / 100) + 48));
+				LCD_data((uint8)(((counter%100) / 10) + 48));
+				LCD_data((uint8)((counter%10) + 48));
+				delay(180);
+			}
 		}
-		else if(data == GPIO_PF4_M){
-			Button_Operation_Mode(PORTF_INDEX, GPIO_PF0_M, Decrement);
+		else if(data == 0x14U){
+			delay(20);
+			if(data == 0x14U){
+				// LCD Decrement
+				if(counter > 0)
+					counter--;
+				LCD_command(CLEAR);
+				LCD_data((uint8)((counter / 100) + 48));
+				LCD_data((uint8)(((counter%100) / 10) + 48));
+				LCD_data((uint8)((counter%10) + 48));
+			}
+			while(data == 0x14U){
+				data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			}
+		}
+		else if(data == 0x0CU){
+			delay(20);
+			while(data == 0x0CU){
+				data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			}		
+			// LCD Reset
+			counter = 0;
+			LCD_command(CLEAR);
+			LCD_data(counter + 48);
+			LCD_data(counter + 48);
+			LCD_data(counter + 48);
 		}
 	}
 }
