7374a6b38a77c7be1fd50c8756eff40c241f03e5
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-03-19 22:55:53 +0200

    main enhancement

--- Main_Driver/main.c
+++ Main_Driver/main.c
@@ -5,11 +5,10 @@
 
 #define PORTA_INDEX					0
 #define PORTB_INDEX					1
-#define ENABLE						  1
-#define RST_BUTTON					0x10U		// Pin 4 is connected to increment switch
-#define DEC_BUTTON					0x08U		// Pin 3 is connected to decrement switch
-#define INC_BUTTON					0x04U		// Pin 2 is connected to Reset switch
-#define PUSH_BUTTONS				0x1CU 	//Pins 0 and 4 are connected to Input Switches
+#define RST_BUTTON					0x0CU		// Pin 4 is connected to increment switch
+#define DEC_BUTTON					0x14U		// Pin 3 is connected to decrement switch
+#define INC_BUTTON					0x18U		// Pin 2 is connected to Reset switch
+#define PUSH_BUTTONS					0x1CU 	//Pins 0 and 4 are connected to Input Switches
 
 void SystemInit(void){
 	PORT_INT(PORTA_INDEX);
@@ -18,33 +17,33 @@ void SystemInit(void){
 	Port_SetPinDirection(PORTA_INDEX, PUSH_BUTTONS, PORT_PIN_IN);
 	Port_SetPinDirection(PORTA_INDEX, 0xE0, PORT_PIN_OUT);
 	LCD_init();
-	Port_SetPinPullUp(PORTA_INDEX,PUSH_BUTTONS, ENABLE);
+	Port_SetPinPullUp(PORTA_INDEX,PUSH_BUTTONS, STD_HIGH);
 }
 
 int main(void){
-	uint8 data;
+	uint8 Data_IN;
 	uint16 counter = 0;
 	LCD_data(48);
 	LCD_data(48);
 	LCD_data(48);
 	while(1){
-		data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
-		if(data == 0x18U){
-			delay(20);
-			data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
-			if(data == 0x18U){
+		Data_IN = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+		if(Data_IN == INC_BUTTON){
+			delay(200);
+			Data_IN = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			if(Data_IN == INC_BUTTON){
 				// LCD Increment
 				counter = (counter+1)%1000;
 				LCD_command(CLEAR);
 				LCD_data((uint8)((counter / 100) + 48));
 				LCD_data((uint8)(((counter%100) / 10) + 48));
 				LCD_data((uint8)((counter%10) + 48));
-				delay(180);
+				// delay(180);
 			}
 		}
-		else if(data == 0x14U){
+		else if(Data_IN == DEC_BUTTON){
 			delay(20);
-			if(data == 0x14U){
+			if(Data_IN == DEC_BUTTON){
 				// LCD Decrement
 				if(counter > 0)
 					counter--;
@@ -53,14 +52,14 @@ int main(void){
 				LCD_data((uint8)(((counter%100) / 10) + 48));
 				LCD_data((uint8)((counter%10) + 48));
 			}
-			while(data == 0x14U){
-				data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			while(Data_IN == DEC_BUTTON){
+				Data_IN = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
 			}
 		}
-		else if(data == 0x0CU){
+		else if(Data_IN == RST_BUTTON){
 			delay(20);
-			while(data == 0x0CU){
-				data = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
+			while(Data_IN == RST_BUTTON){
+				Data_IN = DIO_ReadPort(PORTA_INDEX, PUSH_BUTTONS);
 			}		
 			// LCD Reset
 			counter = 0;
